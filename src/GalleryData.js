ff2e373f66ed58b5fc7774c7591f04f7f807588e
optimist19 <oluwasegunbamidele3@gmail.com>
2023-09-21 20:38:33 +0100

    finally done

--- src/GalleryData.js
+++ src/GalleryData.js
@@ -1,13 +1,13 @@
-import one from './assets/1.jpg'
-import two from './assets/2.jpg'
-import three from './assets/3.jpg'
-import four from './assets/4.jfif'
-import five from './assets/5.jpg'
-import six from './assets/6.jpg'
-import seven from './assets/7.jfif'
-import eight from './assets/8.jpg'
-import nine from './assets/9.jfif'
-import ten from './assets/10.jpg'
+import morgan from './assets/1.jpg'
+import cynthia from './assets/2.jpg'
+import tems from './assets/3.jpg'
+import tops from './assets/4.jfif'
+import imole from './assets/5.jpg'
+import audiX1 from './assets/6.jpg'
+import snack from './assets/7.jfif'
+import noodle from './assets/8.jpg'
+import benz from './assets/9.jfif'
+import lexus from './assets/10.jpg'
 
 
 
@@ -18,51 +18,61 @@ export const GalleryData =[
 	{
 		id: 1,
 		title: 'celebrity',
-		image: one
+		name: 'morgan',
+		image: morgan
 	},
 	{
 		id: 2,
 		title: 'celebrity',
-		image: two
+		name: 'cynthia',
+		image: cynthia
 	},
 	{
 		id: 3,
 		title: 'celebrity',
-		image: three
+		name: 'tems',
+		image: tems
 	},
 	{
 		id: 4,
 		title: 'clothing',
-		image: four
+		name: 'tops',
+		image: tops
 	},
 	{
 		id: 5,
 		title: 'celebrity',
-		image: five
+		name: 'imole',
+		image: imole
 	},
 	{
 		id: 6,
 		title: 'car',
-		image: six
+		name: 'audiX1',
+		image: audiX1
 	},
 	{
 		id: 7,
 		title: 'food',
-		image: seven
+		name: 'snack',
+		image: snack
 	},
 	{
 		id: 8,
 		title: 'food',
-		image: eight
+		name: 'noodle',
+		image: noodle
 	},
 	{
 		id: 9,
 		title: 'car',
-		image: nine
+		name: 'benz',
+		image: benz
 	},
 	{
 		id: 10,
 		title: 'car',
-		image: ten
+		name: 'lexus',
+		image: lexus
 	},
 ]
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,30 @@
-import { useState, useEffect } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { GalleryData } from "../GalleryData";
+import { useState, useEffect } from "react"
+import SortableList, { SortableItem } from 'react-easy-sort'
+import { arrayMoveImmutable } from 'array-move';
 import { signOut, onAuthStateChanged} from "firebase/auth";
 import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
-
 import logout from "../assets/Logout.png";
+//The dnd kit didn't work so ignore the the two folders which are supposed to be exported here
+
+
 
 function Home() {
-  const [data, setData] = useState(GalleryData);
+
+  const [data,setData] = useState([]);
+  const [collection,setCollection] = useState([]);
+  const [searchInput, setSearchInput] = useState('')
   const navTo = useNavigate();
 
-  const gallery_filter = (itemData) => {
-    const filterData = GalleryData.filter((item) => item.title === itemData);
-    setData(filterData);
-  };
 
-  const onDragEnd = (result) => {
-    if (!result.destination) return;
 
-    const items = [...data];
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setData(items);
-  };
 
   useEffect(()=>{
-    // const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
+    setData(GalleryData);
+    setCollection([... new Set(GalleryData.map((item)=> item.title))])
+
+     onAuthStateChanged(auth, (user) => {
       if (user) {
         // const uid = user.uid;
         // ...
@@ -39,9 +35,19 @@ function Home() {
         navTo("/login");
       }
     });
-  },[])
 
-  function logOut() {
+  },[]) 
+
+  const gallery_filter = (itemData) =>{
+    const filterData = GalleryData.filter((item)=> item.title == itemData);
+    setData(filterData);
+  }
+
+  const onSortEnd = (oldIndex, newIndex) => {
+    setData((array) => arrayMoveImmutable(array, oldIndex, newIndex))
+  }
+
+    function logOut() {
     // const auth = getAuth();
     signOut(auth)
       .then(() => {
@@ -54,282 +60,62 @@ function Home() {
       });
   }
 
+
+
+  // console.log(data)
+
+  function searchFtn(e){
+
+    const char = data.filter(a =>{
+      return a.name.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+    setData(char)
+    setSearchInput(e.target.value)
+    console.log(searchInput)
+  }
+
+ 
+
   return (
-    <div className="App">
-      <div className="galleryWrapper">
-        <div className="filterItem">
-          <ul>
-            <li>
-              <button onClick={() => setData(GalleryData)}>All</button>
-            </li>
-            {Array.from(new Set(GalleryData.map((item) => item.title))).map(
-              (item) => (
-                <li key={item}>
-                  <button onClick={() => gallery_filter(item)}>{item}</button>
-                </li>
-              )
-            )}
-          </ul>
-        </div>
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="gallery" direction="horizontal">
-            {(provided) => (
-              <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                className="galleryContainer"
-              >
-                {data.map((item, index) => (
-                  <Draggable
-                    key={item.id}
-                    draggableId={item.id}
-                    index={index}
-                  >
-                    {(provided) => (
-                      <div
-                        className="galleryItem"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <img src={item.image} alt={item.title} />
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div>
+    <div className="container">
       <div className="logout-img" onClick={logOut}>
         <img src={logout} alt="logOut-pic" />
         <p>Logout</p>
+      </div>
+      <div className="">
+        <div className="tab">
+         <button onClick={()=> setData(GalleryData)}>All</button>
+          {
+            collection.map((item)=>{
+              return(
+                <li key={item}>
+                  <button onClick={()=>{gallery_filter(item)}}>{item}</button>
+                </li>
+              )
+            })
+          }
+        </div>
+        <div className="input">
+          <input type="text" placeholder="search" value={searchInput} onChange={searchFtn}/>
+        </div>
+        <div className="sortable-con">
+          <SortableList onSortEnd={onSortEnd} className="list" draggedItemClassName="dragged">
+            {
+              data.map((item)=>(
+                
+                <SortableItem key={item.id}>
+                  <div className="img-con">
+                    <img src={item.image} />
+                    <p>{item.name}</p>
+                  </div>
+                </SortableItem>
+              ))
+            }
+          </SortableList>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { images } from "../images"
-// import { useState, useEffect } from "react"
-
-// function Home() {
-
-//   const [data, setData] = useState([])
-//   const [collection, setCollection] = useState([])
-
-//   useEffect(()=>{
-//     setData(images)
-//     setCollection([... new Set(images.map(item => item.title))])
-//   },[])
-
-// // console.log(data)
-// // console.log(collection)
-
-//   function filter(title){
-//     const filterImages = images.filter(item => item.title == title)
-//     setData(filterImages)
-//   }
-//   return (
-//     <div className="gallery-wrapper">
-//       <div className="filter-item">
-//         <ul>
-//           <li>All</li>
-//           {
-//             collection.map(item => <li key={item.id}><button onClick={()=>filter(item.title)}>{item}</button></li>)
-//           }
-//         </ul>
-//       </div>
-//       <div className="gallery-container">
-//         {
-//           data.map((a) =>{
-//             return(
-//               <div key={a.id} className="gallery-item">
-//                 <img src={a.image} alt={a.title}/>
-//               </div>
-//             )
-//           })
-//           }
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Home
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useState, useEffect } from "react";
-// import { signOut, onAuthStateChanged} from "firebase/auth";
-// import { auth } from "../firebase";
-// import { useNavigate } from "react-router-dom";
-// import logout from "../assets/Logout.png";
-
-// function Home() {
-//   const [images, setImages] = useState([]);
-//   const navTo = useNavigate();
-
-
-//   const handleDrop = (e) => {
-//     e.preventDefault();
-
-//     const newImages = [...images];
-
-//     for (const file of e.dataTransfer.files) {
-//       const imageUrl = URL.createObjectURL(file);
-//       newImages.push(imageUrl);
-//     }
-//     setImages(newImages);
-//   };
-
-//   const handleDragOver = (e) => {
-//     e.preventDefault();
-//   };
-
-//   function logOut() {
-//     // const auth = getAuth();
-//     signOut(auth)
-//       .then(() => {
-//         // Sign-out successful.
-//         alert("Logged Out Successfully");
-//         navTo("/login");
-//       })
-//       .catch((error) => {
-//         console.log(error.message);
-//       });
-//   }
-
-
-//   useEffect(()=>{
-//     // const auth = getAuth();
-//     onAuthStateChanged(auth, (user) => {
-//       if (user) {
-//         // const uid = user.uid;
-//         // ...
-//         console.log('You are in the home comp');
-//       } else {
-//         // User is signed out
-//         // ...
-//         navTo("/login");
-//       }
-//     });
-//   },[])
-
-
-//   return (
-//     <div className="container">
-//       <div className="logout-img" onClick={logOut}>
-//         <img src={logout} alt="logOut-pic" />
-//       </div>
-//       <div className="drop-box" onDrop={handleDrop} onDragOver={handleDragOver}>
-//         <p>Drag and drop images here</p>
-//       </div>
-
-//       {/* <div className="filter">
-//         <input type="text" placeholder="search" value={filters} onChange={(e)=>setFilters(e.target.value)}/>
-//       </div> */}
-//       <div className="image-container">
-//         {images.map((imageUrl, index) => (
-//           <div key="index" className="img-div">
-//             <img
-//               key={index}
-//               src={imageUrl}
-//               alt={`Image ${index}`}
-//               className="image-preview"
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
